ce659717d90cb39aa31d5da5275d8f8fedf31d3f
stc-cow <82161976+stc-cow@users.noreply.github.com>
2025-11-27 11:44:22 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,14 +11,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/King_Abdalaziz_Camel_Festival_Dashboard",
-  assetPrefix: "/King_Abdalaziz_Camel_Festival_Dashboard/",
-};
-
-module.exports = nextConfig;
